27a6bc07ba698858a1341c6268ce81400465bc25
7minutech <elijohnson1107@gmail.com>
2024-06-18 12:47:59 -0500

    Added operate function

--- script.js
+++ script.js
@@ -13,7 +13,18 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
-
+function operate(a,operator,b){
+    switch (operator){
+        case "+":
+            return add(a,b)
+        case "-":
+            return subtract(a,b)
+        case "*":
+            return multiply(a,b)
+        case "/":
+            return divide(a,b)
+    }
+}
 
 
 
